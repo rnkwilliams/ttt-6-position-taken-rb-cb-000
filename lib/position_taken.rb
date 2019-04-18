b88b4c129711084c346db95d9116fa1ed714f3b5
Ranika Williams <rnkprice07@gmail.com>
2019-04-18 20:53:08 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,6 @@
 def position_taken?(board, index)
   if array[index] == "X" || array[index] == "O"
   else
-    array[index] == "" || array[index] == " "
+    array[index] == "" || array[index] == " " || array[index] == "nil"
+  end
 end
