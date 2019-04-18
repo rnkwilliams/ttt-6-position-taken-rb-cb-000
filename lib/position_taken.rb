a3c73d9bdf02ff2fca7a0a10afaba9311a82d9e8
Ranika Williams <rnkprice07@gmail.com>
2019-04-18 20:56:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if array[index] == "X" || array[index] == "O"
-  else
-    array[index] == "" || array[index] == " " || array[index] == "nil"
+  if index == "X" || index == "O"
+  else index == "" || index == " " || index == "nil"
   end
 end
